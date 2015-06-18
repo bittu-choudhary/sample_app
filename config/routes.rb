24dddee74482c65fa78fdde993d474446d63d345
bittu-moldedbits <bittu@moldedbits.com>
2015-06-18 17:53:00 +0530

    added changes in Help and Home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
