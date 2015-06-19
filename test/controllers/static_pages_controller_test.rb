2e4ad7efc32b4c5ec7b48a9b267476c1eaaf9cda
bittu-moldedbits <bittu@moldedbits.com>
2015-06-19 13:07:37 +0530

    eliminate repetions and add a contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   test "should get home" do
     get :home
     assert(true, "this was expected to be true")
     assert_response :success
-   assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+   assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get test" do
+    get :contact
+    assert_response :success
+    assert_select "contact", "Contact | #{@base_title}"
+  end
 end
