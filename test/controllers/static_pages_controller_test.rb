84f51dead2ad4fe38a8ec7aab7bb18509efb5f36
bittu-moldedbits <bittu@moldedbits.com>
2015-06-24 09:51:50 +0530

    corrected data model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+  require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   def setup
@@ -22,6 +22,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  
   test "should get contact" do
     get :contact
     assert_response :success
