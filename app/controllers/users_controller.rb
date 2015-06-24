9291260a7da53ec0d150aad0266ab40207c1cbd4
bittu-moldedbits <bittu@moldedbits.com>
2015-06-24 18:38:43 +0530

    finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		#good, save!
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
 	else
 		render 'new' 
   	end
