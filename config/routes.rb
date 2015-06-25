e7fa4635df419c0afcbe8f09347e4f0038f4b9d8
bittu-moldedbits <bittu@moldedbits.com>
2015-06-25 19:38:11 +0530

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
-  get   'signup'     =>    'users#new'
-  get   'home'    =>    'static_pages#home'
-  get   'contact' =>    'static_pages#contact'
-  get   'help'    =>    'static_pages#help'
-  get   'about'   =>    'static_pages#about'
+  get 'sessions/new'
+
+  get    'signup'  =>    'users#new'
+  get    'home'    =>    'static_pages#home'
+  get    'contact' =>    'static_pages#contact'
+  get    'help'    =>    'static_pages#help'
+  get    'about'   =>    'static_pages#about'
+  get    'login'   =>    'sessions#new'
+  post   'login'   =>    'sessions#create'
+  delete 'logout'  =>    'sessions#destroy'
 
   root 'static_pages#home'
   
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
       assert_template 'users/show'
       assert_not flash.empty?
+      assert is_logged_in?
     end
 end
