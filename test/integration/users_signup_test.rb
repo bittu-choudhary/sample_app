fe15be27630bec8172f58e203ea13168f7a6bf83
bittu-moldedbits <bittu@moldedbits.com>
2015-06-25 12:29:43 +0530

    chapter 7 exercise

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   									password_confirmation: "bar"}
   								end
   	assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do 
@@ -20,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password: "password", password_confirmation: "password"}
       end
       assert_template 'users/show'
+      assert_not flash.empty?
     end
 end
