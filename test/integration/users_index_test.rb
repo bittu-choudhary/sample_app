dd47c561aa1e601bc9ed04b96bfaf4fe3ff825ce
bittu-moldedbits <bittu@moldedbits.com>
2015-06-30 18:03:31 +0530

    Finish user edit, update, index, and destroy actions

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,16 +5,29 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   def setup
-  	@user = users(:dinesh)
+  	@admin = users(:dinesh)
+    @non_admin = users(:gunjan)
   end
 
-  test "index including pagination" do
-  	log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+  	log_in_as(@admin)
   	get users_path
   	assert_template 'users/index'
   	assert_select 'div.pagination'
-  	User.paginate(page:1).each do |user|
+    first_page_of_users = User.paginate(page:1)
+    first_page_of_users.each do |user|
   		assert_select 'a[href=?', users_path(user), text: user.name
-  	end
+      unless user == @admin
+        assert_select 'a[href=?', users_path(user), text: 'delete', method: :delete       
+  	     end
+      end
+  assert_difference 'User.count', -1 do
+    delete user_path(@non_admin)
+     end
+  end
+  test "index as non_admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a',text: 'delete', count: 0
   end
 end
