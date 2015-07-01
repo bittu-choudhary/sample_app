defa606f44861ca7f831813b08a5067099e5d18d
bittu-moldedbits <bittu@moldedbits.com>
2015-07-02 00:07:58 +0530

    Add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get    'signup'  =>    'users#new'
@@ -15,4 +19,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
