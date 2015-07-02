e9f868064467c27876b5e1c6015fe02673bade25
bittu-moldedbits <bittu@moldedbits.com>
2015-07-03 00:45:16 +0530

    added box for user generated microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -4,4 +4,21 @@ class MicropostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: "Lorem ipsum" }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: @micropost
+    end
+    assert_redirected_to login_url
+  end
 end
