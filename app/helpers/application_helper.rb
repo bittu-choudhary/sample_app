4198f07a55fb1124147a3ae1ff284c44dc8e9af1
bittu-moldedbits <bittu@moldedbits.com>
2015-08-19 21:38:23 +0530

    added all files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,3 +8,23 @@ module ApplicationHelper
 		end
 	end
 end
+
+
+
+
+
+#retrive.authenticated?(:saved_doc,token)
+  # def retrieve_document
+  #   token_digest = digest(params[:token])
+  #   @retrieve_document_id = SavedDocumentLink.find_by(saved_doc_digest: token_digest)
+  #   @retrieve_document = SavedDocumentDetail.find_by(saved_document_id: @retrieve_document_id.saved_document_id)
+  # end
+
+  # @link = "http://localhost:3000/api/v1/saved_document_detail/retrieve_document?token=" + @saved_document_token.to_s
+
+  # def authenticated?(attribute, token)
+  #   digest = send("#{attribute}_digest")
+  #   return false if digest.nil?
+  #   BCrypt::Password.new(digest).is_password?(token)
+  # end
+  # puts saved_document_detail.saved_document_link.saved_doc_digest.to_s
